e59c334c2589e61750ff66a5c2b2d856bb5ff596
Anmol Desai <i-anmol@edmodo.com>
2017-04-12 15:55:47 -0700

    rerouted

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ var express = require('express');
 
 // create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// send https to http because openweather does not allow on https
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next(); //proceed normally
+  }else{
+    res.redirect("http://" + req.hostname + req.url);
+  }
+}
 
 //serve the files to the server
 app.use(express.static('public'));
 
 // start the server
-app.listen(3000,function(){
-console.log("express server is running on 3000");
+app.listen(PORT,function(){
+console.log("express server is running on " + PORT);
 });
