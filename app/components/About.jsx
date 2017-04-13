3a38b4e4b705c9df9da8141676c0d793ecae9319
Anmol Desai <i-anmol@edmodo.com>
2017-04-12 21:25:47 -0700

    about page done

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,13 +1,29 @@
 var React = require('react');
 
 // only for non state components where only render is defined
-var About =  (props) => {
-  return (
-    <div>
-      <h3> About component </h3>
-    <p> About page </p>
-    </div>
-  )
+var About = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">
+                About
+            </h1>
+            <p>
+                This is a weather app built on react.
+            </p>
+            <p>
+                Some of the tools used are:
+                <ul>
+                    <li>
+                        <a target="_blank" href="https://facebook.github.io/react">React</a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="http://openweathermap.org">Open weather map api
+                        </a>
+                    </li>
+                </ul>
+            </p>
+        </div>
+    )
 }
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -5,7 +5,8 @@ var Examples = (props) => {
     return (
         <div>
             <h1 className="text-center">
-                Examples</h1>
+                Examples
+            </h1>
             <p>
                 Here are a few example locations to try out
             </p>
